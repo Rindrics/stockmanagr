bd38ea27bf44bf09f849c5c6867e53b7bcd9fe79
Akira Hayashi <smxshxishxad@me.com>
2019-11-19 15:19:03 +0900

    :bug: Remove `dat` from run_ridge_vpa()

--- R/ridge_vpa.R
+++ R/ridge_vpa.R
@@ -5,7 +5,7 @@
 #' @param fcurrent_year Equals to \code{fc.year} in \code{\link[frasyr]{vpa}}
 #' @param ... Other arguments to pass to \code{\link[frasyr]{vpa}}
 #' @export
-run_ridge_vpa <- function(sigma_constraint, lambda, dat, fcurrent_year, ...) {
+run_ridge_vpa <- function(sigma_constraint, lambda, fcurrent_year, ...) {
   cat(paste0("lambda = ", lambda, "..."))
   result <- frasyr::vpa(fc.year          = fcurrent_year,
                         sigma.constraint = sigma_constraint,
